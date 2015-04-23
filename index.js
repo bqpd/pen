bde92782b7d50623e3ddb6444572f6cdd7bd7490
Edward Burnell <eburn@mit.edu>
2015-04-22 23:58:35 -0400

    update boxite version, cleanup site

---
+++ index.js
@@ -0,0 +1,37 @@
+boxite.compile({
+
+header: {
+  title: "Practical\nEducation\nNetwork",
+  image: "https://www.dropbox.com/sh/vtwtm7iefplbx21/AADWhjzxWO99Lc6ujWCwD-E3a/header.jpg?dl=1"
+},
+
+content: [
+
+  "Fewer than <strong>10% of junior high schools</strong> in Ghana contain laboratory equipment for teaching science. The situation is similar in many of Africa's <strong>10 million schools</strong>.",
+
+  "The Practical Education Network (PEN) empowers teachers around the world to engage their students in hands-on science and real world design projects. <strong>We emphasize solving the challenges of STEM education in the developing world</strong> such as large class sizes, scarce material resources, and the novelty of interactive pedagogies.",
+
+  "PEN offers a series of workshops where STEM trainers learn, design, and share hands-on activities which complement the national curriculum and are created from <strong>low-cost</strong> and <strong>locally available</strong> materials.",
+
+
+  "# Our projects",
+
+  "Below are some highlights from our work. For details on any project, follow its links. We'd be delighted to answer any further questions you have at <a href=\"mailto:pen@mit.edu\">pen@mit.edu</a>.",
+
+  "For updates on new and current projects, <a href=\"http://tinyletter.com/\">join our mailing list</a> or find us <a href=\"https://www.facebook.com/practicalenergy\">on facebook</a> and <a href=\"https://twitter.com/PEN_ed\">on twitter</a>.",
+
+  {
+    image: "https://www.dropbox.com/sh/vtwtm7iefplbx21/AABVP4EVS0vingsGae5vqpL-a/refraction.png?dl=1",
+    place: "ghana",
+    caption: 'Hosting [teacher training workshops](http://practicaleducators.wordpress.com/) with JHS teachers and coordinators',
+    date: "2014 to present"
+  },
+
+  {
+    image: "https://www.dropbox.com/sh/vtwtm7iefplbx21/AAAgYOfn5BUiAxymvBY419z-a/fab7.png?dl=1",
+    place: "lima, perú",
+    caption: "Prototyped second-generation solar curriculum at the FAB7 conference",
+    date: "2011 to 2012"
+  },
+
+]})
